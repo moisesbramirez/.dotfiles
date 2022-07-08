5ae9160fe9034947747b05badffc94bfaa25ddcc
Moises Ramirez <moises@retailnext.net>
2022-07-08 16:40:38 -0700

    Strip trailing white space on save?

--- nvim/lua/aus.lua
+++ nvim/lua/aus.lua
@@ -1,6 +1,14 @@
-vim.api.nvim_create_augroup("moises", { clear = true })
-vim.api.nvim_create_autocmd("BufReadPost", {
-  group = "moises",
+local moises = vim.api.nvim_create_augroup("moises", { clear = true })
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd("BufReadPost", {
+  group = moises,
   pattern = "*.hbs",
   command = "set syntax=html",
+})
+
+autocmd("BufWritePre", {
+  group = moises,
+  pattern = "*",
+  command = "%s/\\s\\+$//e",
 })
