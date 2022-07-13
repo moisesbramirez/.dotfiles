fdd51a29a63892ef4811b6a0c777cd92ed70f76b
Moises Ramirez <moises@retailnext.net>
2022-07-13 13:00:05 -0700

    Add syntax highlighting for more languages!

--- nvim/lua/moi/treesitter/init.lua
+++ nvim/lua/moi/treesitter/init.lua
@@ -1,4 +1,5 @@
 require'nvim-treesitter.configs'.setup {
+  ensure_installed = { "bash", "css", "glimmer", "go", "html", "javascript", "json", "ruby", "scss", "typescript" },
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = false,
