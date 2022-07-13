8a3b472a53de4c76e70b6181841cd9a2b61fb124
Moises Ramirez <moises@retailnext.net>
2022-07-12 23:10:04 -0700

    Reorganize nvim configuration
    Inspired by https://github.com/mhartington/dotfiles, it "feels" more
    organized this way and allows for multiple files related to specific
    neovim setting 🙌. Obviously, I had to comment out require line for
    "settings" I have no customization for.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,17 @@
-require('opts')
-require('plugins')
-require('mappings')
-require('aus')
+require "moi.plugins"
+require "moi.options"
+require "moi.mappings"
+-- require "moi.commands"
+-- require "moi.completion"
+-- require "moi.colors"
+
+require "moi.autocmds"
+-- require "moi.folds"
+-- require "moi.filetree"
+-- require "moi.statusline"
+-- require "moi.treesitter"
+-- require "moi.formatting"
+-- require "moi.lsp"
 
 vim.cmd("colorscheme " .. "onedark")
 
