ff8656df5c48ee286b423788c0da71112723bc25
Moises Ramirez <moises.ramirez90@gmail.com>
2022-11-15 21:51:46 -0800

    Improved telescope prompt/results position

--- nvim/lua/moi/telescope/init.lua
+++ nvim/lua/moi/telescope/init.lua
@@ -1,4 +1,12 @@
 local map = require("moi.mappings").map
+require("telescope").setup({
+  defaults = {
+    sorting_strategy = "ascending",
+    layout_config = {
+      prompt_position = "top",
+    },
+  },
+})
 
 map("n", "<Leader>ff", "<Cmd>lua require('telescope.builtin').git_files()<CR>")
 map("n", "<Leader>fg", "<Cmd>lua require('telescope.builtin').live_grep()<CR>")
