5705a6643d4ca6fa81c82fc1707a16a380e6d782
Moises Ramirez <moises.ramirez90@gmail.com>
2022-11-15 21:15:40 -0800

    Consistency

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,16 +1,16 @@
-require "moi.plugins"
-require "moi.options"
-require "moi.mappings"
--- require "moi.commands"
--- require "moi.completion"
-require "moi.colors"
-require "moi.telescope"
+require("moi.plugins")
+require("moi.options")
+require("moi.mappings")
+-- require("moi.commands")
+-- require("moi.completion")
+require("moi.colors")
+require("moi.telescope")
 
-require "moi.autocmds"
--- require "moi.folds"
--- require "moi.filetree"
--- require "moi.statusline"
-require "moi.treesitter"
--- require "moi.formatting"
-require "moi.lsp"
+require("moi.autocmds")
+-- require("moi.folds")
+-- require("moi.filetree")
+-- require("moi.statusline")
+require("moi.treesitter")
+-- require("moi.formatting")
+require("moi.lsp")
 
