b9b86fd397061424ffeef1473666d626e047a60f
Moises Ramirez <moises.ramirez90@gmail.com>
2022-12-08 14:48:34 -0800

    Add auto-import statement support via LSP

--- nvim/lua/moi/lsp/init.lua
+++ nvim/lua/moi/lsp/init.lua
@@ -3,10 +3,17 @@ local lspconfig = require("lspconfig")
 local cmp = require("cmp")
 
 cmp.setup({
-  mapping = {
+  mapping = cmp.mapping.preset.insert({
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
     ["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
     ["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "s" }),
-  },
+  }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
     { name = 'buffer' },
