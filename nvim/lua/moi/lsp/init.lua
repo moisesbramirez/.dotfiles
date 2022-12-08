caae3328d48e2aa4301a3306025f6d19fe4c37e2
Moises Ramirez <moises@retailnext.net>
2022-12-08 13:01:46 -0800

    Consolidate mappings
    - Add mapping for listing diagnostics (Lint Errors)
    - Add mapping to launch lazygit within nvim

--- nvim/lua/moi/lsp/init.lua
+++ nvim/lua/moi/lsp/init.lua
@@ -1,4 +1,4 @@
-local mapBuf = require("moi.mappings").mapBuf
+local lspMappings = require("moi.mappings").lspMappings
 local lspconfig = require("lspconfig")
 local cmp = require("cmp")
 
@@ -13,25 +13,12 @@ cmp.setup({
   })
 })
 
-local on_attach = function(client, bufnr)
-  mapBuf(bufnr, "n", "<Leader>gdc", "<Cmd>lua vim.lsp.buf.declaration()<CR>")
-  mapBuf(bufnr, "n", "<Leader>gd", "<Cmd>lua vim.lsp.buf.definition()<CR>")
-  mapBuf(bufnr, "n", "<Leader>gh", "<Cmd>lua vim.lsp.buf.hover()<CR>")
-  mapBuf(bufnr, "n", "<Leader>gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
-  mapBuf(bufnr, "n", "<Leader>gs", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
-  mapBuf(bufnr, "n", "<Leader>gtd", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
-  mapBuf(bufnr, "n", "<Leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
-  mapBuf(bufnr, "n", "<Leader>gr", "<cmd>lua vim.lsp.buf.references()<CR>")
-  mapBuf(bufnr, "n", "<Leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>")
-  mapBuf(bufnr, "v", "<Leader>ca", "<cmd>lua vim.lsp.buf.range_code_action()<CR>")
-end
-
 local servers = { "ember", "eslint" }
 local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
-    on_attach = on_attach,
+    on_attach = lspMappings,
     capabilities = capabilities
   }
 end
--- nvim/lua/moi/mappings/init.lua
+++ nvim/lua/moi/mappings/init.lua
@@ -16,10 +16,33 @@ function M.mapBuf(buf, mode, lhs, rhs, opts)
   vim.api.nvim_buf_set_keymap(buf, mode, lhs, rhs, options)
 end
 
+-- LSP Mappings
+function M.lspMappings(client, bufnr)
+  M.mapBuf(bufnr, "n", "<Leader>gdc", "<Cmd>lua vim.lsp.buf.declaration()<CR>")
+  M.mapBuf(bufnr, "n", "<Leader>gd", "<Cmd>lua vim.lsp.buf.definition()<CR>")
+  M.mapBuf(bufnr, "n", "<Leader>gh", "<Cmd>lua vim.lsp.buf.hover()<CR>")
+  M.mapBuf(bufnr, "n", "<Leader>gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
+  M.mapBuf(bufnr, "n", "<Leader>gs", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
+  M.mapBuf(bufnr, "n", "<Leader>gtd", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
+  M.mapBuf(bufnr, "n", "<Leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
+  M.mapBuf(bufnr, "n", "<Leader>gr", "<cmd>lua vim.lsp.buf.references()<CR>")
+  M.mapBuf(bufnr, "n", "<Leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>")
+  M.mapBuf(bufnr, "v", "<Leader>ca", "<cmd>lua vim.lsp.buf.range_code_action()<CR>")
+end
+
 -- Swiching between splits/panes
 M.map("n", "<c-k>", ":wincmd k<CR>", { silent = true })
 M.map("n", "<c-j>", ":wincmd j<CR>", { silent = true })
 M.map("n", "<c-h>", ":wincmd h<CR>", { silent = true })
 M.map("n", "<c-l>", ":wincmd l<CR>", { silent = true })
+
+-- Telescope mappings
+M.map("n", "<Leader>ff", "<Cmd>lua require('telescope.builtin').git_files()<CR>")
+M.map("n", "<Leader>fg", "<Cmd>lua require('telescope.builtin').live_grep()<CR>")
+M.map("n", "<Leader>fb", "<Cmd>lua require('telescope.builtin').buffers()<CR>")
+M.map("n", "<Leader>xd", "<Cmd>lua require('telescope.builtin').diagnostics()<CR>")
+
+-- Run lazygit
+M.map("n", "<Leader>lg", "<Cmd>terminal lazygit<CR>")
 
 return M
