fbe84574bcb95f5158fdffb7af8589b6b0a569f7
Moises Ramirez <moises.ramirez90@gmail.com>
2022-12-08 19:59:11 -0800

    Add mapping to buffer within Telescope's buffers

--- nvim/lua/moi/telescope/init.lua
+++ nvim/lua/moi/telescope/init.lua
@@ -1,9 +1,18 @@
 require("telescope").setup({
   defaults = {
-    sorting_strategy = "ascending",
     layout_config = {
       prompt_position = "top",
     },
+    sorting_strategy = "ascending",
   },
+  pickers = {
+    buffers = {
+      mappings = {
+        n = {
+          ["xx"] = require("telescope.actions").delete_buffer
+        },
+      },
+    },
+  }
 })
 
