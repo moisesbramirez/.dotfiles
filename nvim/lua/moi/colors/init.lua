22544fe5e6f41c498767895cc64a78e2c82eb253
Moises Ramirez <moises.ramirez90@gmail.com>
2023-01-18 20:01:09 -0800

    Transparency for accountability 🤡

--- nvim/lua/moi/colors/init.lua
+++ nvim/lua/moi/colors/init.lua
@@ -1,5 +1,14 @@
 local M = {}
 
+require("tokyonight").setup({
+  transparent = true,
+  terminal_colors = true,
+  styles = {
+    sidebars = "transparent",
+    floats = "transparent",
+  },
+})
+
 vim.cmd("colorscheme ".."tokyonight")
 
 function M.setItalics()
