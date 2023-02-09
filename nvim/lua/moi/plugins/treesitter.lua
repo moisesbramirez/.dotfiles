b96cad0e8047652a84bccaa55848069c042d3bbe
Moises Ramirez <moises.ramirez90@gmail.com>
2023-02-09 10:21:46 -0800

    Add tree sitter for improved syntax highlighting
    Shamelessly swiped the config. from:
    https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/plugins/treesitter.lua

---
+++ nvim/lua/moi/plugins/treesitter.lua
@@ -0,0 +1,48 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    version = false, -- last release is way too old and doesn't work on Windows
+    build = ":TSUpdate",
+    event = { "BufReadPost", "BufNewFile" },
+    keys = {
+      { "<c-space>", desc = "Increment selection" },
+      { "<bs>", desc = "Schrink selection", mode = "x" },
+    },
+    opts = {
+      highlight = { enable = true, additional_vim_regex_highlighting = false },
+      indent = { enable = true },
+      context_commentstring = { enable = true, enable_autocmd = false },
+      ensure_installed = {
+        "bash",
+        "css",
+        "glimmer",
+        "go",
+        "help",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "regex",
+        "ruby",
+        "scss",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = "<nop>",
+          node_decremental = "<bs>",
+        },
+      },
+    },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+    end,
+  },
+}
