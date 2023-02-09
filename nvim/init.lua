9d374c5405e616a837dedb55822e34a6766e8827
Moises Ramirez <moises.ramirez90@gmail.com>
2023-02-08 23:17:17 -0800

    Plan to use lazy.nvim for plugin management
    Remove these since we have to set them up differently.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,14 +1,4 @@
-require("moi.plugins")
 require("moi.options")
 require("moi.mappings")
-require("moi.colors")
-require("moi.telescope")
-
 require("moi.autocmds")
--- require("moi.folds")
--- require("moi.filetree")
--- require("moi.statusline")
-require("moi.treesitter")
--- require("moi.formatting")
-require("moi.lsp")
 
--- nvim/lua/moi/treesitter/init.lua
+++
@@ -1,9 +0,0 @@
-require("nvim-treesitter.configs").setup({
-  ensure_installed = { "bash", "css", "glimmer", "go", "html", "javascript", "json", "ruby", "scss", "typescript" },
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
-  indent = { enable = true },
-})
-
