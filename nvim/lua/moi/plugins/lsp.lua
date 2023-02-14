f8c7a105625478568765151aeb3ca2005f7ef29e
Moises Ramirez <moises.ramirez90@gmail.com>
2023-02-14 13:57:11 -0800

    Add LSP and auto complete plugins
    We're using `lsp-zero.nvim`'s installation instructions as is:
    https://github.com/VonHeikemen/lsp-zero.nvim#installing
    
    I took the config. from:
    https://github.com/VonHeikemen/lsp-zero.nvim/discussions/147#discussion-4832039

---
+++ nvim/lua/moi/plugins/lsp.lua
@@ -0,0 +1,53 @@
+return {
+  {
+    "VonHeikemen/lsp-zero.nvim",
+    branch = "v1.x",
+    dependencies = {
+      -- LSP Support
+      {"neovim/nvim-lspconfig"},             -- Required
+      {"williamboman/mason.nvim"},           -- Optional
+      {"williamboman/mason-lspconfig.nvim"}, -- Optional
+
+      -- Autocompletion
+      {"hrsh7th/nvim-cmp"},         -- Required
+      {"hrsh7th/cmp-nvim-lsp"},     -- Required
+      {"hrsh7th/cmp-buffer"},       -- Optional
+      {"hrsh7th/cmp-path"},         -- Optional
+      {"saadparwaiz1/cmp_luasnip"}, -- Optional
+      {"hrsh7th/cmp-nvim-lua"},     -- Optional
+
+      -- Snippets
+      {"L3MON4D3/LuaSnip"},             -- Required
+      {"rafamadriz/friendly-snippets"}, -- Optional
+    },
+    config = function()
+      vim.opt.signcolumn = "yes"
+      local lsp = require("lsp-zero")
+      lsp.preset("recommended")
+      lsp.setup()
+
+      -- diagnostic config
+      vim.diagnostic.config({
+        virtual_text = true,
+        signs = true,
+        update_in_insert = false,
+        underline = true,
+        severity_sort = false,
+        float = {
+          focusable = false,
+          style = "minimal",
+          border = "",
+          source = "always",
+          header = "",
+          prefix = "",
+        },
+      })
+    end,
+  },
+  {
+    "j-hui/fidget.nvim",
+    config = function()
+      require("fidget").setup({})
+    end,
+  },
+}
--- nvim/lua/moi/plugins/treesitter.lua
+++ nvim/lua/moi/plugins/treesitter.lua
@@ -21,6 +21,7 @@ return {
         "html",
         "javascript",
         "json",
+        "jsonc",
         "lua",
         "markdown",
         "markdown_inline",
