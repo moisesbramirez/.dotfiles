ab0406d67ea2fe48ed4980405302bdfdb6173200
Moises Ramirez <moises.ramirez90@gmail.com>
2023-05-11 11:38:44 -0700

    Brighten line numbers and comments
    Tired of switching themes when working outside. Finally figure out to
    customize plugins through Lazy.

--- nvim/lua/moi/plugins/tokyonight.lua
+++ nvim/lua/moi/plugins/tokyonight.lua
@@ -4,15 +4,19 @@ return {
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     opts = {
+      style = "night",
       transparent = true,
       styles = {
-        comments = { italic = true },
         sidebars = "transparent",
         floats = "transparent",
       },
+      on_highlights = function(hl, c)
+        hl.Comment = { fg = "#7986cc" }
+        hl.LineNr = { fg = "#94a3e7" }
+      end,
     },
-    config = function()
-      -- load the colorscheme here
+    config = function(_, opts)
+      require("tokyonight").setup(opts);
       vim.cmd([[colorscheme tokyonight-moon]])
     end,
   },
