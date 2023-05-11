80f906c0366cf97a3ca777a19f38afecb25161f3
Moises Ramirez <moises.ramirez90@gmail.com>
2023-05-10 23:14:57 -0700

    Add lualine plugin
    The default status line is fine for me, I'm adding this because having
    "diagnostics" values at a glace saves me from having to squash commits.
    
    Drive-by: Update plugins

--- nvim/lua/moi/plugins/editor.lua
+++ nvim/lua/moi/plugins/editor.lua
@@ -6,4 +6,25 @@ return {
       vim.o.timeoutlen = 300
     end,
   },
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      options = {
+        icons_enabled = false,
+        component_separators = '',
+        section_separators = '',
+      },
+      sections = {
+        lualine_a = {},
+        lualine_b = { "branch" },
+        lualine_c = { "filename", "diagnostics" },
+        lualine_x = { "encoding" },
+        lualine_y = { "location", "progress"  },
+        lualine_z = {}
+      },
+    },
+    config = function(_, opts)
+      require("lualine").setup(opts)
+    end,
+  },
 }
